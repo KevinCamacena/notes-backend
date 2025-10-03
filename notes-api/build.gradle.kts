7b82af4b8d06cc0e78646ba97cb68758a89b9d98
Kevin Camacena <kevincamacenabenoit@gmail.com>
2025-10-02 19:24:15 -0600

    Starting project

---
+++ notes-api/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.notesapp"
+version = "0.0.1-SNAPSHOT"
+description = "A simple notes API"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
