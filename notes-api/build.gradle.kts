18fdfa23933e0a9256d72237019443f4203f41b8
Kevin Camacena <kevincamacenabenoit@gmail.com>
2025-10-19 10:54:32 -0600

    Fix: change depends_on in notes-api and adding dependency spring-boot-starter-actuator

--- notes-api/build.gradle.kts
+++ notes-api/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 
